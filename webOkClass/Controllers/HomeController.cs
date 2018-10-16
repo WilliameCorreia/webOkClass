be1519790a28041f8154de149a0c51c98c6da3c1
williame <williame_lima@hotmail.com>
2018-10-16 16:02:33 -0300

    Correções links das view e os Controls

--- webOkClass/Controllers/HomeController.cs
+++ webOkClass/Controllers/HomeController.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace webOkClass.Controllers
 {
     public class HomeController : Controller
@@ -17,6 +15,16 @@ namespace webOkClass.Controllers
         }
 
         public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult RecuperarSenha()
+        {
+            return View();
+        }
+
+        public IActionResult PaginaPrincipal()
         {
             return View();
         }
